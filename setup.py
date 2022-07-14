168e987d0da6649b346ca3aa0481c74568aa41b9
Rory Black <100487573+roryblakc@users.noreply.github.com>
2022-07-14 11:10:23 -0400

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'MonteCarloPackage',
+    version = '1.0.0',
+    packages = ['MonteCarlo'],
+    author = "Rory Black",
+    author_email = 'qmn9tb@virginia.edu',
+    description = 'Implementation of a simple Monte Carlo simulator using a set of related classes.',
+    url = 'https://github.com/roryblakc/FinalProject-DS5100',
+    license = 'MIT'
+)
